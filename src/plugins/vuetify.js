1879b79ee0a4c1a84d6ef7739e01114d90dae0a0
Alexey Polishchuk <a.polishchuk@ze.studio>
2022-07-17 19:10:48 +0300

    updated vuetify config

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,20 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
+const defaultLightTheme = {
+  primary: colors.lightBlue.darken2,
+};
+
 export default new Vuetify({
+  icons: {
+    iconfont: "mdiSvg",
+  },
+  theme: {
+    themes: {
+      light: defaultLightTheme,
+    },
+  },
 });
